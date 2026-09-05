e240c0b291411543954ec4a45738d5f4b7a437f9
Utkarsh Kumar Gupta <utkarshg2033@gmail.com>
2026-09-05 13:34:40 +0530

    Login endpoint and SecurityConfig complete

--- src/main/java/org/utkarsh/taskmanager/config/SecurityConfig.java
+++ src/main/java/org/utkarsh/taskmanager/config/SecurityConfig.java
@@ -2,19 +2,46 @@ package org.utkarsh.taskmanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.utkarsh.taskmanager.service.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private UserDetailsService userDetailsService;
+
+    SecurityConfig(UserDetailsService userDetailsService){
+        this.userDetailsService=userDetailsService;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider=new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 
     @Bean
@@ -25,7 +52,9 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                          )
-                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .sessionManagement(session->session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider());
 
         return http.build();
 
--- src/main/java/org/utkarsh/taskmanager/controller/UserController.java
+++ src/main/java/org/utkarsh/taskmanager/controller/UserController.java
@@ -1,11 +1,13 @@
 package org.utkarsh.taskmanager.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.utkarsh.taskmanager.dto.LoginUser;
 import org.utkarsh.taskmanager.dto.RegisterUser;
 import org.utkarsh.taskmanager.model.User;
 import org.utkarsh.taskmanager.service.UserService;
@@ -29,6 +31,12 @@ public class UserController {
         newuser.setPassword(hashedPassword);
         newuser.setUsername(user.getUsername());
         service.registerUser(newuser);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody LoginUser user){
+        service.loginUser(user);
         return ResponseEntity.ok().build();
     }
 }
