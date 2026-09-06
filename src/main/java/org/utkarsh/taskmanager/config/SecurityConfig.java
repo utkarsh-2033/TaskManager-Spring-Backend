4da058432877e2275f0aa5791f1e63c84e5acfec
Utkarsh Kumar Gupta <utkarshg2033@gmail.com>
2026-09-06 13:20:47 +0530

    Add OpenAPI dependency and permit access to Swagger UI endpoints

--- src/main/java/org/utkarsh/taskmanager/config/SecurityConfig.java
+++ src/main/java/org/utkarsh/taskmanager/config/SecurityConfig.java
@@ -49,7 +49,10 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                          )
                 .sessionManagement(session->session
